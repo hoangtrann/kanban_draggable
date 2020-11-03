e6a0f5e6e14bde7b37d26de4a71e88bbb0ffb5d9
Hoang Tran <thhoang.tr@gmail.com>
2020-11-03 18:15:58 +0700

    [IMP] Rework implementation

--- kanban_draggable/__manifest__.py
+++ kanban_draggable/__manifest__.py
@@ -7,33 +7,44 @@
 Disable DragDrop/Sorting In Specific Kanban View
 ================================================
  To disable drag drop record between columns add:
- 
+
   disable_drag_drop_record="true" into the <kanban> tag.
 
  To disable drag drop and sorting records add :
 
   disable_sort_record="true" into the <kanban> tag.
-  
+
  To disable sorting columns add :
 
   disable_sort_column="true" into the <kanban> tag.
 
- Example: 
- 
+ Example:
+
   <kanban disable_sort_column='true' disable_sort_record='true' disable_drag_drop_record='true'>
-        
+
    ...
 
    ...
-        
+
   </kanban>
+
+  This is a fork repository from NavyBits since the original repository is
+  inactive for awhile.
+
+  This repository fix the issue with attribute value in inherited view. Now you
+  use inherited view to modify kanban options.
+
+  ```
+    <xpath expr="//kanban" position="attributes">
+        <attribute name="disable_drag_drop_record">True</attribute>
+    </xpath>
+  ```
     """,
 
-    'version': '1.0',
+    'version': '11.0.0.2',
     'category': 'Extra Tools',
     'summary': 'Provides the ability to control drag drop and sorting behavior in kanban views.',
-    'author': 'Mahmoud Fakhreddine,'
-              'Navybits',
+    'author': 'Mahmoud Fakhreddine, NavyBits; Hoang Tran',
     "website": "https://navybits.com/",
     'depends': [
         'web',
--- kanban_draggable/static/src/js/kanban_renderer.js
+++ kanban_draggable/static/src/js/kanban_renderer.js
@@ -1,48 +1,47 @@
-odoo.define('kanban_draggable.kanban_renderer',function(require){
-"use strict";
+odoo.define("kanban_draggable.kanban_renderer", function (require) {
+    "use strict";
 
+    var KanbanRenderer = require("web.KanbanRenderer");
 
-var KanbanRenderer = require('web.KanbanRenderer');
-
-KanbanRenderer.include({
-
-    _setState: function (state) {
-        this._super.apply(this, arguments);
-
-        var arch = this.arch;
-        var drag_drop = true;
-        if (arch.attrs.disable_drag_drop_record) {
-            if (arch.attrs.disable_drag_drop_record=='true') {
-                this.columnOptions.draggable = false;
+    KanbanRenderer.include({
+        _setState: function (state) {
+            this._super.apply(this, arguments);
+            var self = this;
+            var arch = this.arch;
+            var attrs = [
+                "disable_drag_drop_record",
+                "disable_sort_record",
+                "disable_sort_column",
+            ];
+            if (arch) {
+                attrs.forEach(function (item) {
+                    if (!_.has(arch.attrs, item)) {
+                        return;
+                    }
+                    switch (item) {
+                        case "disable_drag_drop_record":
+                            self.columnOptions.draggable = !JSON.parse(
+                                arch.attrs[item].toLowerCase()
+                            );
+                        case "disable_sort_record":
+                            self.recordOptions.sortable = !JSON.parse(
+                                arch.attrs[item].toLowerCase()
+                            );
+                        case "disable_sort_column":
+                            self.columnOptions.sortable = !JSON.parse(
+                                arch.attrs[item].toLowerCase()
+                            );
+                    }
+                });
             }
-        }
+        },
 
-        this.recordOptions.sortable = true;
-        if (arch.attrs.disable_sort_record) {
-            if (arch.attrs.disable_sort_record=='true') {
-                this.recordOptions.sortable = false;
+        _renderGrouped: function (fragment) {
+            this._super.apply(this, arguments);
+
+            if (!this.columnOptions.sortable) {
+                this.$el.sortable("disable");
             }
-        }
-
-        this.columnOptions.sortable = true;
-        if (arch.attrs.disable_sort_column) {
-            if (arch.attrs.disable_sort_column=='true') {
-                this.columnOptions.sortable = false;
-            }
-        }
-    },
-
-    _renderGrouped: function (fragment) {
-        this._super.apply(this, arguments);
-
-        if (this.columnOptions.sortable==false){
-            this.$el.sortable( "disable" );
-        }
-
-    },
-
-
-
-});
-
+        },
+    });
 });
